ff4b90e138987356b066a2d121aa66c91781ba45
Jessica Lei <jessicalei2022@u.northwestern.edu>
2021-01-20 20:05:53 -0600

    dates on bar chart

--- components/WeeklyMacroChart.js
+++ components/WeeklyMacroChart.js
@@ -1,25 +1,39 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { StackedBarChart } from "react-native-svg-charts";
+import { StackedBarChart, XAxis } from "react-native-svg-charts";
 import dailySumming from "../utils/dailySumming";
 import theme from "../utils/theme";
 
 const WeeklyMacroChart = ({ log, foodResults }) => {
   const data = dailySumming(log, foodResults);
+  //console.log(data);
   const colors = [theme.red, theme.orange, theme.darkYellow];
   const keys = ["protein", "carbohydrate", "fat"];
+  const contentInset = { top: 30, bottom: 10 };
   return (
     <View>
       <Text>Weekly Summary</Text>
-      <StackedBarChart
-        style={{ height: 200 }}
-        animate={false}
-        keys={keys}
-        colors={colors}
-        data={data}
-        showGrid={false}
-        contentInset={{ top: 30, bottom: 30 }}
-      />
+      <div>
+        <StackedBarChart
+          style={{ height: 200, flex: 1}}
+          animate={false}
+          keys={keys}
+          colors={colors}
+          data={data}
+          showGrid={false}
+          contentInset={contentInset}
+        />
+        <XAxis
+          //style={{ flex: 1}}
+          data={data}
+          xAccessor={({ item }) => Math.floor(item.date)}
+          contentInset={{ top: 5, bottom: 30, left: 7, right: 7 }}
+          svg={{
+              fill: 'grey',
+              fontSize: 10,
+          }}
+        />
+      </div>
     </View>
   );
 };
--- utils/dailySumming.js
+++ utils/dailySumming.js
@@ -21,11 +21,13 @@ const findIdInFoodResults = (id, foodResults) => {
 };
 
 const dailySumming = (log, foodResults) => {
+  console.log(log);
   const recentDates = Array(7)
     .fill()
     .map((_, index) => {
       let date = new Date();
       let temp = new Date(date.setDate(date.getDate() - 6 + index));
+      //console.log(temp);
       return temp.toJSON();
     });
 
@@ -40,10 +42,13 @@ const dailySumming = (log, foodResults) => {
         const nutrients =
           foodResults[findIdInFoodResults(logItem.fdcId, foodResults)].foods[0]
             .foodNutrients;
+        let currDay = new Date(logItem.time);
+        //console.log(currDay.getDate());
         return {
           protein: nutrients[PROTEIN].value * PROTEIN_CAL * logItem.grams,
           fat: nutrients[FAT].value * FAT_CAL * logItem.grams,
           carb: nutrients[CARB].value * CARB_CAL * logItem.grams,
+          date: currDay.getDate(),
         };
       })
       .reduce(
@@ -52,15 +57,18 @@ const dailySumming = (log, foodResults) => {
             protein: total.protein + logItem.protein,
             carbohydrate: total.carbohydrate + logItem.carb,
             fat: total.fat + logItem.fat,
+            date: logItem.date,
           };
         },
         {
           protein: 0,
           carbohydrate: 0,
           fat: 0,
+          date: new Date().getDate(),
         }
       );
   });
+  console.log(data);
   return data;
 };
 export default dailySumming;
